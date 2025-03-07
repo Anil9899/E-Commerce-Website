3ab4f2e3d88b23bb86f927c51e7afdedfac9faf1
Anil9899 <ay19869@gmail.com>
2025-03-07 11:53:09 +0530

    initial file

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
   <div className='navbar'>
     <div className="nav-logo">
         <img src={logo} alt="" />
-        <p>SHOPPING</p>
+        <p>E-SHOP</p>
     </div>
 
   </div>
